23baff5b455a4290fd5215a20b47175082c09062
Alexander Verbeek <sanderman@gmail.com>
2016-08-03 21:26:37 +0200

    Demonstrated calling back into C++ from Haskell by passing in a function pointer for later use. This is necessary because we cannot dynamically link in both directions as that would create a circular dependency.

--- haskell/src/Hello.hs
+++ haskell/src/Hello.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- Hello.hs
 module Hello where
 
-foreign export ccall helloFromHaskell :: IO ()
+import Foreign
 
-helloFromHaskell :: IO ()
+foreign export ccall helloFromHaskell :: IO ()
 helloFromHaskell = putStrLn "Hello from Haskell"
+
+
+foreign import ccall "dynamic" mkHello :: FunPtr (IO ()) -> IO ()
+
+foreign export ccall registerHelloFromC :: FunPtr (IO ()) -> IO ()
+registerHelloFromC :: FunPtr (IO ()) -> IO ()
+registerHelloFromC func = do
+  -- cast/convert our function pointer into a proper haskell function
+  let hello = mkHello func
+  -- normally we would use this moment to store it into some global storage for later use
+  -- but today we'll just invoke the function right away to demonstrate it works
+  hello
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -10,11 +10,22 @@ extern "C" {
 	void HsEnd();
 }
 
+void helloFromC() {
+	std::cout << "Hello from C++\n";
+}
+
 int main(int argc, char** argv) {
 	HsStart();
-	std::cout << "Hello from C++\n";
+	helloFromC();
 	helloFromHaskell();
+
+	// register the helloFromC function in the main application to the haskell plugin library.
+	// normally, the plugin should store the function pointer for later use, but since these are example projects, 
+	// for now we will immediately invoke the function.
+	registerHelloFromC(helloFromC);
+
 	HsEnd();
 	std::cin.get();
 	return 0;
 }
+
